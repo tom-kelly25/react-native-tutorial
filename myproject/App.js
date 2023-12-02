eb5c797724b936bd3bc495aee9fc5b2ab9b69099
tom-kelly25 <107217874+tom-kelly25@users.noreply.github.com>
2023-12-02 14:33:12 +0000

    Lesson 4 using state

--- myproject/App.js
+++ myproject/App.js
@@ -1,17 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("shaun");
+  const [person, setPerson] = useState({ name: "mario", age: 40 });
+
+  const clickHandler = () => {
+    setName("chun-li");
+    setPerson({ name: "luigi", age: 45 });
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello test tom!</Text>
-      </View>
-      <View style={styles.body}>
-        <Text> Lorem ipsum dolor sit amet.</Text>
-        <Text> Lorem ipsum dolor sit amet.</Text>
-        <Text> Lorem ipsum dolor sit amet.</Text>
-        <Text> Lorem ipsum dolor sit amet.</Text>
+      <Text>My name is {name}</Text>
+      <Text>
+        His name is {person.name} and his age is {person.age}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="update state" onPress={clickHandler} />
       </View>
     </View>
   );
@@ -20,19 +26,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
+    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
-  header: {
-    backgroundColor: "pink",
-    padding: 20,
-  },
-  boldText: {
-    fontWeight: "bold",
-  },
-  body: {
-    backgroundColor: "white",
-    padding: 20,
+  buttonContainer: {
+    marginTop: 20,
   },
 });
